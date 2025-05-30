5c771c1c55690c08e799155e94a453b18ddb2bea
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-05-30 21:03:39 +0700

    fix(next-config): fixing turbopack problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
-  webpack: (config) => {
-    // Only disable minimization in development
-    if (process.env.NODE_ENV !== "production") {
-      config.optimization.minimize = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
