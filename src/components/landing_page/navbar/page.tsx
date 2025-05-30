f9c7fce6e470845ed99e30c68ea66f775ba60167
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-05-30 23:11:33 +0700

    feat(component): updating navbar mechanism

--- src/components/landing_page/navbar/page.tsx
+++ src/components/landing_page/navbar/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
-import ButtonPink from "@/components/button_pink/page";
-import ButtonWhite from "@/components/button_white/page";
-import NavText from "../../navbar_text/page";
 import Link from "next/link";
+import ButtonPink from "@/components/button_pink/page"; // Untuk tombol Daftar
+import ButtonWhite from "@/components/button_white/page"; // Untuk tombol Masuk
+import NavText from "../../navbar_text/page";
+import { useAuth } from "@/lib/auth"; // Import useAuth
 
 const Navbar = () => {
+  const { isAuthenticated, loading } = useAuth();
+
   return (
     <div className="sticky top-0 z-50 w-full h-24 bg-[#FAFAFA] bg-opacity-80 backdrop-blur-md px-4 md:px-24 flex items-center justify-between shadow-sm transition-all duration-300">
       <Link href="/">
@@ -19,10 +22,24 @@ const Navbar = () => {
         <NavText text="FAQ" href="#faq" />
       </div>
       <div className="flex items-center">
-        <div className="mr-3">
-          <ButtonPink />
-        </div>
-        <ButtonWhite />
+        {!loading &&
+          (isAuthenticated ? (
+            <Link href="/dashboard">
+              <div
+                className="px-8 py-4 bg-[var(--color-p-300)] rounded-4xl cursor-pointer transition-all duration-300 group
+                hover:bg-[var(--color-w-300)] border border-transparent hover:border-[var(--color-p-300)]"
+              >
+                <p className="text-[#FAFAFA] font-semibold transition-colors duration-300 group-hover:text-[var(--color-p-300)]">Dashboard</p>
+              </div>
+            </Link>
+          ) : (
+            <>
+              <div className="mr-3">
+                <ButtonPink />
+              </div>
+              <ButtonWhite />
+            </>
+          ))}
       </div>
     </div>
   );
